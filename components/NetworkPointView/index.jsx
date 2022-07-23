efe7838525f3dea25af38e1834622a93937df68e
gfidelisdev <109247627+gfidelisdev@users.noreply.github.com>
2022-07-23 16:46:57 -0300

    real time network point state

--- components/NetworkPointView/index.jsx
+++ components/NetworkPointView/index.jsx
@@ -1,13 +1,18 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { forwardRef, useImperativeHandle, useContext, useEffect, useRef, useState } from "react";
 import { getNetworkPointState } from "../../contexts/PingEventsProvider/actions";
 import { PingEventsContext } from "../../contexts/PingEventsProvider/context";
 import styles from "./networkPointView.module.css";
-function NetworkPointView({ network_point }) {
+
+const NetworkPointView = forwardRef(({ network_point }, ref) => {
+
   let [nState, setNState] = useState('');
   let [bgColor, setBgColor] = useState('');
   let [showPingEvents, setShowPingEvents] = useState('')
-  // let [lastPingEvents, setLastPingEvents] = useState([])
-  let localPingEvents = [];
+  let [localPingEvents, setLocalPingEvents] = useState([]);
+  let firstrender = true
+  function getNwpId(){
+    return network_point.id
+  }  
   const getNetworkPointState = async (nwpoint_id) => {
     const lastPingEvents = await fetch(
       `http://localhost:8080/api/ping_events?nwpoint_id=${nwpoint_id}&limit=10`
@@ -27,33 +32,38 @@ function NetworkPointView({ network_point }) {
       }
       return "success";
     };
+
+    const mockCheckState=(ping_events)=>{
+      let mockResult = Math.random()
+      if (mockResult>0.7) return 'danger'
+      if (mockResult>0.3) return 'warning'
+      return 'success'
+    }
     let nwp_state = checkState(await pingEvents);
+    // let nwp_state = mockCheckState(await pingEvents);
     return {nwp_state, pingEvents};
   };
-  useEffect(() => {
+
+  useImperativeHandle(ref, ()=>({
+    reloadNwpState(){
+      // console.log(network_point.id)
+      firstrender = false
+      populatePingEvents()
+    },
+    
+  }))
+
+  function populatePingEvents(){
     getNetworkPointState(network_point.id).then((data) => {
       setNState(data.nwp_state);
       setBgColor(`bg-${data.nwp_state}-color`);
-      localPingEvents = data.pingEvents
+      setLocalPingEvents(data.pingEvents)
     });
-  }, []);
-  // useEffect(() => {
-  //   getNetworkPointState(pingEventsDispatch,network_point.id)
-  //   bg_color = `bg-${pingEventsState.nwp_state}-color`
-  //   console.log(network_point.name,bg_color, pingEventsState)
+  }
+  useEffect(() => {
+    populatePingEvents()
+  }, [firstrender]);
 
-  //   return () => {
-
-  //   }
-  // }, [pingEventsDispatch])
-
-  // useEffect(() => {
-  //   // console.log('ping_events_state',pingEventsState.nwp_state)
-  //   // console.log(Styles[`bg-${pingEventsState.nwp_state}-color`])
-  //   // console.log(bg_color)
-  //   return () => {
-  //   }
-  // }, [pingEventsState])
 
   function getLastNresults() {
     console.log(localPingEvents)
@@ -64,23 +74,28 @@ function NetworkPointView({ network_point }) {
   }
 
   function viewPingEvents(show){
-    console.log(localPingEvents)
     setShowPingEvents(show)
   }
   return (
     <div
-      className={`flex justify-center rounded
+      className={`flex justify-center rounded flex-col
                      p-2 m-2 text-black text-lg
-                     uppercase font-bold 
+                     uppercase font-bold  
                      ${styles[bgColor]}`}
       onClick={(e) => showStats(e)}
       onMouseEnter={()=>viewPingEvents(true)}
       onMouseLeave={()=>viewPingEvents(false)}
     >
-      {network_point.name}
-      <div className={showPingEvents?'':'hidden'}></div>
+      <div className="">{network_point.name}</div>
+      <div className={`flex justify-between bg-white ring items-center content-between ${showPingEvents?'':'hidden'}`}>
+        {localPingEvents.map(pe=>{
+          return (
+            <span key={pe.id} className={`${styles['bg-'+pe.type+'-color']} w-[10%]`}>&#160;</span>
+          )
+        })}
+      </div>
     </div>
   );
-}
+})
 
 export default NetworkPointView;
--- components/NetworkPoints/index.jsx
+++ components/NetworkPoints/index.jsx
@@ -1,27 +1,38 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { loadNetworkPoints } from "../../contexts/NetworkPointsProvider/actions";
 import { NetworkPointsContext } from "../../contexts/NetworkPointsProvider/context";
 import NetworkPointView from "../NetworkPointView";
+import socketClient from "socket.io-client";
 
 function NetworkPoints() {
-    
-    const networkPointsContext = useContext(NetworkPointsContext)
-    const {networkPointsState, networkPointsDispatch} = networkPointsContext
+  const networkPointsContext = useContext(NetworkPointsContext);
+  const { networkPointsState, networkPointsDispatch } = networkPointsContext;
+  const nwpView = useRef([])
 
+  var socket = socketClient("http://localhost:8080");
+  socket.on("msg", (msg) => {
+    if (typeof window !== 'undefined') {
+      nwpView.current.forEach(nwp=>{
+        nwp?.reloadNwpState()
+      })
+    }
+  });
 
-    useEffect(()=>{
-        loadNetworkPoints(networkPointsDispatch)
-    },[networkPointsDispatch])
-    
+  useEffect(() => {
+    loadNetworkPoints(networkPointsDispatch);
+  }, [networkPointsDispatch]);
+
   return (
     <div className="flex flex-wrap w-full justify-around content-between items-center">
-        {networkPointsState.network_points.map(nwp=>{
-            return(
-                <NetworkPointView key={nwp.id} network_point={nwp}/>
-            ) 
-        })}
+      {networkPointsState.network_points.map((nwp) => {
+        return (
+          <div key={nwp.id}>
+            <NetworkPointView ref={el=>nwpView.current[nwp.id]=el} network_point={nwp} />
+          </div>
+        );
+      })}
     </div>
-)
+  );
 }
 
 export default NetworkPoints;
